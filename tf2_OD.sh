0c897b8b6c7d2ca735a8b01ab38e1d467d79c915
ashishpatel26 <shriganesh.patel@gmail.com>
2021-12-23 16:22:41 +0530

    tf2_od.sh added

---
+++ tf2_OD.sh
@@ -0,0 +1,9 @@
+mkdir -p Tensorflow
+cd Tensorflow/
+git clone --depth 1 https://github.com/tensorflow/models
+cd models/research/
+pip install --upgrade pip
+protoc object_detection/protos/*.proto --python_out=.
+cp object_detection/packages/tf2/setup.py .
+python -m pip install --use-feature=2020-resolver .
+python /content/Tensorflow/models/research/object_detection/builders/model_builder_tf2_test.py
