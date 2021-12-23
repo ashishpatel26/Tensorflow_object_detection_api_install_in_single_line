fe27915694935a0e9d8a757584774240c6f312c8
ashishpatel26 <shriganesh.patel@gmail.com>
2021-12-23 16:29:38 +0530

    updated

--- tf2_OD.sh
+++ tf2_OD.sh
@@ -5,5 +5,5 @@ cd models/research/
 pip install --upgrade pip
 protoc object_detection/protos/*.proto --python_out=.
 cp object_detection/packages/tf2/setup.py .
-python -m pip install --use-feature=2020-resolver .
+python -m pip install .
 python /content/Tensorflow/models/research/object_detection/builders/model_builder_tf2_test.py
